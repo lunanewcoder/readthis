dce4cbeda331583c53f6dd5decab87cdf2d4a723
lunanewcoder <86455540+lunanewcoder@users.noreply.github.com>
2021-06-25 18:31:17 -0500

    Update MathTest.java

--- MathTest.java
+++ MathTest.java
@@ -5,27 +5,38 @@ public class interger {
 	public static void main(String[] args) {
      
 		//STRING A ='s STORING SOMETHING IN THE "".
+		//Limit to #math.random() + int "" = 0.999999999
+		// 0 - 100 Is goal from using getScore()
 		String a = "Java: MATH (CODED IN)"; 
 		int i = 3;
 		int b = 7;
 		int c = 2;
 		int d = 8;
-		int e = 12;
+		int e = 12; 
+		int f = 54;
+		int g = 32;
+		int h = 68;
+		
 		System.out.println(a);
 		System.out.println(i);
 		System.out.println(b);
 		System.out.println(c);
 		System.out.println(d);
 		System.out.println(e);
-
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+		
 		System.out.println(i + b);
 		System.out.println(b + c);
 		System.out.println(c + d);
 	    System.out.println(d + e);
-	    
+	    System.out.println(f + g);
+	    System.out.println(g + h);
         
         String name = "This was a fun time to code an it means the world to me to when you run this so thank you";
         System.out.println(name.toLowerCase());
+         
 
 	    
 
@@ -34,3 +45,6 @@ public class interger {
 
 }
 
+//CODED BY SEAMORE
+
+//Make a new java project, AND DO NOT USE A DEFAULT PACKAGE. Make a new package named "Project", Make a new class called "interger" (inside the project package) then paste this code in.
