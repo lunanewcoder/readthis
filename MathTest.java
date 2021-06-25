2f4e4ab89a228c5c688b911d6e58c90ed597ebbe
lunanewcoder <86455540+lunanewcoder@users.noreply.github.com>
2021-06-25 15:20:32 -0500

    Update MathTest.java

--- MathTest.java
+++ MathTest.java
@@ -5,22 +5,32 @@ public class interger {
 	public static void main(String[] args) {
      
 		//STRING A ='s STORING SOMETHING IN THE "".
-		String a = "Java: Random Number Dice (CODED IN)"; 
+		String a = "Java: MATH (CODED IN)"; 
 		int i = 3;
 		int b = 7;
 		int c = 2;
 		int d = 8;
+		int e = 12;
 		System.out.println(a);
 		System.out.println(i);
 		System.out.println(b);
 		System.out.println(c);
 		System.out.println(d);
+		System.out.println(e);
 
 		System.out.println(i + b);
 		System.out.println(b + c);
 		System.out.println(c + d);
+	    System.out.println(d + e);
+	    
+        
+        String name = "This was a fun time to code an it means the world to me to when you run this so thank you";
+        System.out.println(name.toLowerCase());
+
+	    
+
+	    
 	}
 
 }
 
-//CODED BY SEAMORE/LUNA
