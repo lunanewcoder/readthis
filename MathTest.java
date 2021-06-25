f3e5463fc1b545bd39e29cc2a487177f39873035
lunanewcoder <86455540+lunanewcoder@users.noreply.github.com>
2021-06-25 14:02:21 -0500

    Create MathTest.java

---
+++ MathTest.java
@@ -0,0 +1,26 @@
+package Project;
+
+public class interger {
+
+	public static void main(String[] args) {
+     
+		//STRING A ='s STORING SOMETHING IN THE "".
+		String a = "Java: Random Number Dice (CODED IN)"; 
+		int i = 3;
+		int b = 7;
+		int c = 2;
+		int d = 8;
+		System.out.println(a);
+		System.out.println(i);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+
+		System.out.println(i + b);
+		System.out.println(b + c);
+		System.out.println(c + d);
+	}
+
+}
+
+//CODED BY SEAMORE/LUNA
